05dfb9dbeed3d620369b1bb63981520a5fb3e140
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-04 14:29:01 +0800

    提交代码

--- service-main/src/main/java/study/springcloud/provider/support/SpringBootCfg.java
+++ service-main/src/main/java/study/springcloud/provider/support/SpringBootCfg.java
@@ -1,7 +1,10 @@
 package study.springcloud.provider.support;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = "study.springcloud.provider")
 public class SpringBootCfg {
 }
