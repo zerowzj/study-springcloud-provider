ff19534f387f09bfaf1f1200d7bc1e5811baf0fb
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-04 13:56:02 +0800

    提交代码

--- service-main/src/main/java/study/springcloud/provider/support/exception/GlobalExceptionHandler.java
+++ service-main/src/main/java/study/springcloud/provider/support/exception/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
 package study.springcloud.provider.support.exception;
 
-import com.google.common.collect.Maps;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -12,7 +12,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Throwable.class)
     public Map<String, Object> handle(Throwable ex) {
         ex.printStackTrace();
-        Map<String, Object> result = Maps.newHashMap();
+        Map<String, Object> result = new HashMap<>();
         result.put("code", "9999");
         result.put("desc", ex.getMessage());
         return result;
