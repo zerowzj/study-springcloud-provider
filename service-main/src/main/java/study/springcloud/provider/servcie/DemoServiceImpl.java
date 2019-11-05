7d1837257d91bf192e2e14bb1364234acb8c7c35
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-05 09:12:20 +0800

    提交代码

--- service-main/src/main/java/study/springcloud/provider/servcie/DemoServiceImpl.java
+++ service-main/src/main/java/study/springcloud/provider/servcie/DemoServiceImpl.java
@@ -1,13 +1,18 @@
 package study.springcloud.provider.servcie;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 import study.springcloud.provider.DemoService;
 
 @RestController
 public class DemoServiceImpl implements DemoService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DemoServiceImpl.class);
+
     @Override
     public String hello(String name) {
+        LOGGER.info("SFFFFFFFFFFFFFFF");
             return null;
     }
 }
