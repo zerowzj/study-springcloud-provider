39bf04e4f2c3e1705b005f76f8d440d5d1bac0a2
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-09 14:53:34 +0800

    更新代码

--- service-main/src/main/java/study/springcloud/provider/support/SpringBootCfg.java
+++ service-main/src/main/java/study/springcloud/provider/support/SpringBootCfg.java
@@ -1,7 +1,6 @@
 package study.springcloud.provider.support;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
