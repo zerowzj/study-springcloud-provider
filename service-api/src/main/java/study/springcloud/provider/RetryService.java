314bd28dbebfb031360df514f3234d72cce80f27
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-12 12:36:51 +0800

    更新代码

--- service-api/src/main/java/study/springcloud/provider/RetryService.java
+++ service-api/src/main/java/study/springcloud/provider/RetryService.java
@@ -1,8 +1,17 @@
 package study.springcloud.provider;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletResponse;
 
 @FeignClient(name = "study-springcloud-provider")
 public interface RetryService {
+
+    @PostMapping("/404")
+    void retry404(HttpServletResponse response);
+
+    @PostMapping("/500")
+    void retry500(HttpServletResponse response);
 
 }
--- service-main/src/main/java/study/springcloud/provider/servcie/RetryServiceImpl.java
+++ service-main/src/main/java/study/springcloud/provider/servcie/RetryServiceImpl.java
@@ -1,6 +1,28 @@
 package study.springcloud.provider.servcie;
 
+import org.springframework.web.bind.annotation.RestController;
 import study.springcloud.provider.RetryService;
 
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
 public class RetryServiceImpl implements RetryService {
+
+    @Override
+    public void retry404(HttpServletResponse response) {
+        try {
+            response.sendError(400);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void retry500(HttpServletResponse response) {
+        try {
+            response.sendError(500);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
