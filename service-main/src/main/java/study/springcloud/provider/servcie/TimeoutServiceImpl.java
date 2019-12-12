bdd12a07aae6f930034c30ca2b78472bc6f5db34
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-12 11:08:42 +0800

    更新代码

--- service-main/src/main/java/study/springcloud/provider/servcie/TimeoutServiceImpl.java
+++ service-main/src/main/java/study/springcloud/provider/servcie/TimeoutServiceImpl.java
@@ -2,10 +2,12 @@ package study.springcloud.provider.servcie;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
 import study.springcloud.provider.TimeoutService;
 
 import java.util.concurrent.TimeUnit;
 
+@RestController
 public class TimeoutServiceImpl implements TimeoutService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TimeoutServiceImpl.class);
