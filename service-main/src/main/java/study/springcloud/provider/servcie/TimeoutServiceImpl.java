bd3f9e0d7b6fbefdb9b4a65b1d8088bf2c630420
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-12 13:38:28 +0800

    提交代码

--- service-main/src/main/java/study/springcloud/provider/servcie/TimeoutServiceImpl.java
+++ service-main/src/main/java/study/springcloud/provider/servcie/TimeoutServiceImpl.java
@@ -14,7 +14,7 @@ public class TimeoutServiceImpl implements TimeoutService {
 
     @Override
     public String await(Long timeout) {
-        LOGGER.info("收到请求");
+        LOGGER.info("receive the request: {} s", timeout);
         long start = System.currentTimeMillis();
         try {
             TimeUnit.SECONDS.sleep(timeout);
