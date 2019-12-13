5740c02ea89797798bb597c2eee45673599113bb
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-13 11:22:39 +0800

    更新代码

--- service-main/src/main/java/study/springcloud/provider/servcie/RetryServiceImpl.java
+++ service-main/src/main/java/study/springcloud/provider/servcie/RetryServiceImpl.java
@@ -2,16 +2,15 @@ package study.springcloud.provider.servcie;
 
 import org.springframework.web.bind.annotation.RestController;
 import study.springcloud.provider.RetryService;
-
-import javax.servlet.http.HttpServletResponse;
+import study.springcloud.provider.support.util.HttpServlets;
 
 @RestController
 public class RetryServiceImpl implements RetryService {
 
     @Override
-    public void retry(int code, HttpServletResponse response) {
+    public void retry(int code) {
         try {
-            response.sendError(code);
+            HttpServlets.getResponse().sendError(code);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
