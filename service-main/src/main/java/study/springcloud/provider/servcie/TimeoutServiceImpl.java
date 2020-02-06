36dd5a0747c4f92b822757018cfb902c4694c253
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-02-06 23:16:45 +0800

    更新代码

--- service-main/src/main/java/study/springcloud/provider/servcie/TimeoutServiceImpl.java
+++ service-main/src/main/java/study/springcloud/provider/servcie/TimeoutServiceImpl.java
@@ -1,8 +1,6 @@
 package study.springcloud.provider.servcie;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 import study.springcloud.provider.TimeoutService;
 
