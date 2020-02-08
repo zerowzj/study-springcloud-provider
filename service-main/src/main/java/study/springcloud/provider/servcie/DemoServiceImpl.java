d3831f495692a5aa49c85f649994bf77330f985a
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-02-08 17:55:32 +0800

    更新代码

--- service-main/src/main/java/study/springcloud/provider/servcie/DemoServiceImpl.java
+++ service-main/src/main/java/study/springcloud/provider/servcie/DemoServiceImpl.java
@@ -1,9 +1,6 @@
 package study.springcloud.provider.servcie;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import study.springcloud.provider.DemoService;
