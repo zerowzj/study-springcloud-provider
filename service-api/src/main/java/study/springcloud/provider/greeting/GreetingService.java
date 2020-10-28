9e23691ae7edc80bcdd1783fa68317c25b70f08f
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-10-28 16:46:07 +0800

    更新代码

--- service-api/src/main/java/study/springcloud/provider/greeting/GreetingService.java
+++ service-api/src/main/java/study/springcloud/provider/greeting/GreetingService.java
@@ -1,11 +1,10 @@
 package study.springcloud.provider.greeting;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(name = "study-springcloud-provider")
+@RequestMapping("/greeting")
 public interface GreetingService {
 
     @RequestMapping("/sayHi")
