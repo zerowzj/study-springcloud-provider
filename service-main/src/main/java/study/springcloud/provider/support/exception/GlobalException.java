f5c79983f00258b4d0c21c27b7058dfaa5cd1716
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-12-15 14:55:54 +0800

    更新代码

--- service-main/src/main/java/study/springcloud/provider/support/exception/GlobalException.java
+++ service-main/src/main/java/study/springcloud/provider/support/exception/GlobalException.java
@@ -1,34 +1,34 @@
-package study.springcloud.provider.support.exception;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import javax.servlet.http.HttpServletRequest;
-
-@Configuration
-public class GlobalException extends ResponseEntityExceptionHandler {
-    private final static Logger logger = LoggerFactory.getLogger(ResponseEntityExceptionHandler.class);
-
-    @ExceptionHandler
-    @ResponseBody
-    ResponseEntity<?> handleControllerException(HttpServletRequest request, Throwable ex) {
-        logger.info("{}", ex.getClass().getName());
-        logger.error("全局异常:",ex);
-        HttpStatus status = getStatus(request);
-        return new ResponseEntity<String>(status);
-    }
-
-    private HttpStatus getStatus(HttpServletRequest request) {
-        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
-        if (statusCode == null) {
-            return HttpStatus.INTERNAL_SERVER_ERROR;
-        }
-        return HttpStatus.valueOf(statusCode);
-    }
-}
+//package study.springcloud.provider.support.exception;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+//
+//import javax.servlet.http.HttpServletRequest;
+//
+//@Configuration
+//public class GlobalException extends ResponseEntityExceptionHandler {
+//    private final static Logger logger = LoggerFactory.getLogger(ResponseEntityExceptionHandler.class);
+//
+//    @ExceptionHandler
+//    @ResponseBody
+//    ResponseEntity<?> handleControllerException(HttpServletRequest request, Throwable ex) {
+//        logger.info("{}", ex.getClass().getName());
+//        logger.error("全局异常:",ex);
+//        HttpStatus status = getStatus(request);
+//        return new ResponseEntity<String>(status);
+//    }
+//
+//    private HttpStatus getStatus(HttpServletRequest request) {
+//        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+//        if (statusCode == null) {
+//            return HttpStatus.INTERNAL_SERVER_ERROR;
+//        }
+//        return HttpStatus.valueOf(statusCode);
+//    }
+//}
--- service-main/src/main/java/study/springcloud/provider/support/exception/GlobalExceptionHandler.java
+++ service-main/src/main/java/study/springcloud/provider/support/exception/GlobalExceptionHandler.java
@@ -1,23 +1,23 @@
-package study.springcloud.provider.support.exception;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@Slf4j
-@RestControllerAdvice
-public class GlobalExceptionHandler {
-
-    @ExceptionHandler(Throwable.class)
-    public Map<String, Object> handle(Throwable ex) {
-        log.info("provider 异常了");
-        ex.printStackTrace();
-        Map<String, Object> result = new HashMap<>();
-        result.put("code", "8989");
-        result.put("desc", ex.getMessage());
-        return result;
-    }
-}
+//package study.springcloud.provider.support.exception;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.RestControllerAdvice;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Slf4j
+//@RestControllerAdvice
+//public class GlobalExceptionHandler {
+//
+//    @ExceptionHandler(Throwable.class)
+//    public Map<String, Object> handle(Throwable ex) {
+//        log.info("provider 异常了");
+//        ex.printStackTrace();
+//        Map<String, Object> result = new HashMap<>();
+//        result.put("code", "8989");
+//        result.put("desc", ex.getMessage());
+//        return result;
+//    }
+//}
